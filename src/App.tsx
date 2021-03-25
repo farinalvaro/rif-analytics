87d731434fc805be0e2f0c69ca8c742cdad6df6b
Alvaro Fariña <farinalvaro@gmail.com>
2021-03-25 17:39:36 -0300

    feat: adds tracker component to opt-in tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tracker from './components/Tracker';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Sandbox project for RIF Analytics
         </p>
+        <Tracker />
       </header>
     </div>
   );
